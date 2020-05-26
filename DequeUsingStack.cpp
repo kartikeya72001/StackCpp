327871d184d7471516e1fd6519acb8b1f8e175d5
kartikeya72001 <kartikeya72001@gmail.com>
2020-05-26 13:21:34 +0530

    corrections

---
+++ DequeUsingStack.cpp
@@ -0,0 +1,13 @@
+// Implement a Queue using two stacks Make it Dequeue efficient.
+//
+// Input Format
+// Enter the size of the queue N add 0 - N-1 numbers in the queue
+//
+// Constraints
+// Output Format
+// Display the numbers in the order they are dequeued and in a space separated manner
+//
+// Sample Input
+// 5
+// Sample Output
+// 0 1 2 3 4
